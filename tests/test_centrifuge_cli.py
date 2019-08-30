4e5760a8eaa66eb29932991d9ac703c54e927628
Peter Eacmen <peacmen@refirmlabs.com>
2019-08-30 00:55:39 -0400

    buttoning up travis, adding unit test to check for version consistency between toml and __init.py

--- tests/test_centrifuge_cli.py
+++ tests/test_centrifuge_cli.py
@@ -1,5 +1,14 @@
 from centrifuge_cli import __version__
 
+import toml
+from pathlib import Path
+
+
+def get_version():
+    path = Path(__file__).resolve().parents[1] / 'pyproject.toml'
+    pyproject = toml.loads(open(str(path)).read())
+    return pyproject['tool']['poetry']['version']
+
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == get_version()
